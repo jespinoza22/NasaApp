23b0354db6c096760b8512122a8bd89acfd86f32
junior espinoza <jespinoza2292@gmail.com>
2020-07-29 21:40:22 -0500

    adding changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NasaApp';
+
+  selectMenu(){
+    const menu = document.querySelector('.menu-header');
+    if (menu.classList.contains('NUEVA')){
+      menu.classList.remove('NUEVA');
+    }else {
+      menu.classList.add('NUEVA');
+    }
+
+    console.log(menu);
+  }
 }
